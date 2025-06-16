c6cf34506023f35858086d25d86b8509e36a374f
t-nil <t-nil@t-nil.dev>
2025-06-16 20:16:58 +0200

    chart: kill noise to the side (make alignment of used space with `:auto` better)

--- src/Chart.cpp
+++ src/Chart.cpp
@@ -222,8 +222,8 @@ std::pair <int, int> Chart::determineHourRange (
   }
   else
   {
-    first_hour = std::max (first_hour - 1, 0);
-    last_hour = std::min (last_hour, 23);
+    first_hour = std::max (first_hour, 0);
+    last_hour = std::min (last_hour + 1, 23);
   }
 
   return std::make_pair (first_hour, last_hour);
